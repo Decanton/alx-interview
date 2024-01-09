450fbf87c55d96db8f1d99b77ccac36be605c224
owuordickens0001@gmail.com <owuordickens0001@gmail.com>
2024-01-09 10:55:31 +0000

    Lockboxes uses BFS

---
+++ 0-lockboxes.py/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    if not boxes or not boxes[0]:
+        return False
+
+    n1 = len(boxes)
+    visited = [False] * n1
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+
+        for key in boxes[current_box]:
+            if 0 <= key < n1 and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
